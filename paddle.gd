024e0c1792bde4ce4b2ce6414a07987f3fbbf4c0
SecretShinobi <marc.lunar@mail.utoronto.ca>
2024-05-28 10:29:09 -0400

    first commit

---
+++ paddle.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_axis("player_one_move_up", "player_one_move_down")
+	position.y += direction * 500 * delta
+	
