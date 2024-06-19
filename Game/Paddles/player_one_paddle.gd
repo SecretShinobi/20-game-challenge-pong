263d78dfb4d4fad3b7d9e5496d89f5380b06d88d
SecretShinobi <marc.lunar@mail.utoronto.ca>
2024-06-18 22:22:26 -0400

    Added player movement and modified ball
    Added player movement for paddles and started working on implementing the angled bouncing movement for the ball

---
+++ Game/Paddles/player_one_paddle.gd
@@ -0,0 +1,17 @@
+class_name PlayerOne
+extends CharacterBody2D
+## This is the class used to configure player one's parameters
+
+const SPEED = 500.0
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("player_one_move_up", "player_one_move_down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
