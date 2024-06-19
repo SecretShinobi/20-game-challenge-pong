84d43aca78c21a38a9a1a39e688b5c382b781e02
Marc <44608766+SecretShinobi@users.noreply.github.com>
2024-06-18 22:23:30 -0400

    Finish Rewriting (#2)
    * Added Extra Improvements
    
    Further adjusted file structure and added extra graphic fixes
    
    * Added player movement and modified ball
    
    Added player movement for paddles and started working on implementing the angled bouncing movement for the ball

--- Game/Ball/Ball.gd
+++ Game/Ball/Ball.gd
@@ -1,6 +1,9 @@
 class_name Ball
 extends CharacterBody2D
 ## A ball that is bounced around on the screen in a top-down manner
+##
+## This is the ball that is bounced around between the two players in the pong
+## game. The movement and behavior of the ball can be adjusted here
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,3 +16,6 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
+		# Change the bounce angle if ball hits paddle based on dist. from center
+		if collision.get_collider() is CharacterBody2D:
+			print(collision.get_position())
